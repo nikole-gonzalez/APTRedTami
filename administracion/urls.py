ebc61c10f3147346d8e725de38c9b116fea40ab1
nikole <niko.gonzalez@duocuc.cl>
2025-04-23 11:23:54 -0400

    e

---
+++ administracion/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView 
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='admin-index'),
+    path('admin_index/', views.admin_index, name='admin_index'),
+    path('cesfam_index/', views.cesfam_index, name='cesfam_index'),
+   
+
+    path('respuestas/', views.respuestas, name='respuestas'),
+    path('reportes/', views.reportes, name='reportes'),
+    path('apis/', views.apis, name='apis'),
+    path('mensaje/', views.mensaje, name='mensajes'),
+
+    path('datos_perfil/', views.datos_perfil, name="datos_perfil"),
+    path('tamizaje/', views.tamizaje, name="tamizaje")
+   
+]
---
+++ administracion/views.py
@@ -0,0 +1,66 @@
+
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'administracion/index.html')
+
+def admin_index(request):
+    return render(request, 'administracion/index.html')
+
+def cesfam_index(request):
+    return render(request, 'cesfam/index_c.html')
+
+
+
+
+
+def respuestas(request):
+    return render(request, 'administracion/respuestas.html')
+
+@login_required
+def reportes(request):
+    return render(request, 'administracion/reportes.html')
+
+def apis(request):
+    return render(request, 'administracion/apis.html')
+
+def mensaje(request):
+    return render(request, 'administracion/mensaje.html')
+
+def datos_perfil(request):
+    return render(request, 'administracion/datos_perfil.html')
+
+def tamizaje(request):
+    return render(request, 'administracion/tamizaje.html')
+
+
+
+# ------------------------------------------------------ #
+# ---------------------- Reportes ---------------------- #
+# ------------------------------------------------------ #
+
+# Configuración global para fuentes de gráficos
+"""
+plt.rcParams['font.family'] = 'sans-serif'  
+plt.rcParams['font.sans-serif'] = 'Calibri' 
+plt.rcParams['font.size'] = 11
+plt.rcParams['axes.titlesize'] =20
+plt.rcParams['axes.labelsize']= 13
+plt.rcParams['axes.labelpad']=10
+
+# Función para convertir los gráficos a base64
+"""
+"""
+def convertir_grafico_a_base64():
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+    buffer.seek(0)
+    plt.close()
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+#Así se usa
+imagen_base64 = convertir_grafico_a_base64()
+return imagen_base64
+"""
