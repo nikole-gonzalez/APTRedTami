e0ba2f75996b63c43057d6ac756a59583bc15e49
Constanza Zúñiga <cons.zunigap@duocuc.cl>
2025-04-24 12:22:37 -0400

    Eliminar archivos cacheados del seguimiento de Git

--- core/settings.py
+++ core/settings.py
@@ -129,3 +129,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # Para producción
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'redtamicacu@gmail.com'  
+EMAIL_HOST_PASSWORD = 'vngr zlbp gjnu ullc' 
