1a104aa0cf915c31249d7a04e1af665cba0eb485
nikole <niko.gonzalez@duocuc.cl>
2025-05-07 16:43:20 -0400

    reportes y admin

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,3 +1,118 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display=("id_perfil", "telefono", "cod_acceso", "fecha_creacion", "user", "usuario_sist")
+    search_fields=("id_perfil", "telefono", "cod_acceso", "fecha_creacion", "user", "usuario_sist")
+    list_filter=("id_perfil", "telefono", "cod_acceso", "fecha_creacion", "user", "usuario_sist")
+    
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display=("id_manychat","rut_usuario","dv_rut","fecha_nacimiento","num_whatsapp","fecha_ingreso","cod_comuna")
+    search_fields=("id_manychat","rut_usuario","dv_rut","fecha_nacimiento","num_whatsapp","fecha_ingreso","cod_comuna")
+    list_filter=("id_manychat","rut_usuario","dv_rut","fecha_nacimiento","num_whatsapp","fecha_ingreso","cod_comuna")
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display=("cod_region","nombre_region")
+    search_fields=("cod_region","nombre_region")
+    list_filter=("cod_region","nombre_region")
+
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display=("cod_provincia","nombre_provincia","cod_region")
+    search_fields=("cod_provincia","nombre_provincia","cod_region")
+    list_filter=("cod_provincia","nombre_provincia","cod_region")
+
+class ComunaAdmin(admin.ModelAdmin):
+    list_display=("cod_comuna","nombre_comuna","cod_provincia")
+    search_fields=("cod_comuna","nombre_comuna","cod_provincia")
+    list_filter=("cod_comuna","nombre_comuna","cod_provincia")
+
+class UsuarioTextoPreguntaAdmin(admin.ModelAdmin):
+    list_display=("id_texto_preg","texto_pregunta","fecha_pregunta_texto","id_usuario")
+    search_fields=("id_texto_preg","texto_pregunta","fecha_pregunta_texto","id_usuario")
+    list_filter=("id_texto_preg","texto_pregunta","fecha_pregunta_texto","id_usuario")
+
+class PregTMAdmin(admin.ModelAdmin):
+    preg_tm = models.CharField(max_length=200)
+    list_display=("id_preg_tm","preg_tm","cod_pregunta_tm")
+    search_fields=("id_preg_tm","preg_tm","cod_pregunta_tm")
+    list_filter=("id_preg_tm","preg_tm","cod_pregunta_tm")
+
+class OpcTMAdmin(admin.ModelAdmin):
+    list_display=("id_opc_tm","opc_resp_tm","id_preg_tm")
+    search_fields=("id_opc_tm","opc_resp_tm","id_preg_tm")
+    list_filter=("id_opc_tm","opc_resp_tm","id_preg_tm")
+
+class RespTMAdmin(admin.ModelAdmin):
+    list_display=("id_resp_tm","id_manychat","fecha_respuesta_tm","id_opc_tm","id_usuario")
+    search_fields=("id_resp_tm","id_manychat","fecha_respuesta_tm","id_opc_tm","id_usuario")
+    list_filter=("id_resp_tm","id_manychat","fecha_respuesta_tm","id_opc_tm","id_usuario")
+
+class PregDSAdmin(admin.ModelAdmin):
+    list_display=("id_preg_ds","preg_ds","cod_pregunta_ds")
+    search_fields=("id_preg_ds","preg_ds","cod_pregunta_ds")
+    list_filter=("id_preg_ds","preg_ds","cod_pregunta_ds")
+
+class OpcDSAdmin(admin.ModelAdmin):
+    list_display=("id_opc_ds","opc_resp_ds","id_preg_ds")
+    search_fields=("id_opc_ds","opc_resp_ds","id_preg_ds")
+    list_filter=("id_opc_ds","opc_resp_ds","id_preg_ds")
+
+class RespDSAdmin(admin.ModelAdmin):
+    list_display=("id_resp_ds","id_manychat","fecha_respuesta_ds","id_opc_ds","id_usuario")
+    search_fields=("id_resp_ds","id_manychat","fecha_respuesta_ds","id_opc_ds","id_usuario")
+    list_filter=("id_resp_ds","id_manychat","fecha_respuesta_ds","id_opc_ds","id_usuario")
+
+class PregFRMAdmin(admin.ModelAdmin):
+    list_display=("id_preg_frm","preg_frm","cod_pregunta_frm")
+    search_fields=("id_preg_frm","preg_frm","cod_pregunta_frm")
+    list_filter=("id_preg_frm","preg_frm","cod_pregunta_frm")
+
+class OpcFRMAdmin(admin.ModelAdmin):
+    list_display=("id_opc_frm","opc_resp_frm","id_preg_frm")
+    search_fields=("id_opc_frm","opc_resp_frm","id_preg_frm")
+    list_filter=("id_opc_frm","opc_resp_frm","id_preg_frm")   
+
+class RespFRMAdmin(admin.ModelAdmin):
+    list_display=("id_resp_frm","id_manychat","fecha_respuesta_frm","id_opc_frm","id_usuario")
+    search_fields=("id_resp_frm","id_manychat","fecha_respuesta_frm","id_opc_frm","id_usuario")
+    list_filter=("id_resp_frm","id_manychat","fecha_respuesta_frm","id_opc_frm","id_usuario")
+
+class PregFRNMAdmin(admin.ModelAdmin):
+    list_display=("id_preg_frnm","preg_frnm","cod_pregunta_frnm")
+    search_fields=("id_preg_frnm","preg_frnm","cod_pregunta_frnm")
+    list_filter=("id_preg_frnm","preg_frnm","cod_pregunta_frnm")
+
+class OpcFRNMAdmin(admin.ModelAdmin):
+    list_display=("id_opc_frnm","opc_resp_frnm","id_preg_frnm")
+    search_fields=("id_opc_frnm","opc_resp_frnm","id_preg_frnm")
+    list_filter=("id_opc_frnm","opc_resp_frnm","id_preg_frnm")
+
+class RespFRNMAdmin(admin.ModelAdmin):
+    list_display=("id_resp_frnm","id_manychat","fecha_respuesta_frnm","id_opc_frnm","id_usuario")
+    search_fields=("id_resp_frnm","id_manychat","fecha_respuesta_frnm","id_opc_frnm","id_usuario")
+    list_filter=("id_resp_frnm","id_manychat","fecha_respuesta_frnm","id_opc_frnm","id_usuario")
+
+class DivulgacionAdmin(admin.ModelAdmin):
+    list_display=("id_divultacion","texto_divulgacion","url","fecha_envio","imagen")
+    search_fields=("id_divultacion","texto_divulgacion","url","fecha_envio","imagen")
+    list_filter=("id_divultacion","texto_divulgacion","url","fecha_envio","imagen")
+
+admin.site.register(PerfilUsuario, PerfilUsuarioAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Provincia, ProvinciaAdmin)
+admin.site.register(Comuna, ComunaAdmin)
+admin.site.register(UsuarioTextoPregunta, UsuarioTextoPreguntaAdmin)
+admin.site.register(PregTM, PregTMAdmin)
+admin.site.register(OpcTM, OpcTMAdmin)
+admin.site.register(RespTM, RespTMAdmin)
+admin.site.register(PregDS, PregDSAdmin)
+admin.site.register(OpcDS, OpcDSAdmin)
+admin.site.register(RespDS, RespDSAdmin)
+admin.site.register(PregFRM, PregFRMAdmin)
+admin.site.register(OpcFRM, OpcFRMAdmin)
+admin.site.register(RespFRM, RespFRMAdmin)
+admin.site.register(PregFRNM, PregFRNMAdmin)
+admin.site.register(OpcFRNM, OpcFRNMAdmin)
+admin.site.register(RespFRNM, RespFRNMAdmin)
+admin.site.register(Divulgacion, DivulgacionAdmin)
